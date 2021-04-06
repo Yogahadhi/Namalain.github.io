8cd39066420cf9166e46da0c6f5b8635b997ddc3
Yoga Triatmojo Hadhi <yogazen137@gmail.com>
2021-04-07 00:37:47 +0800

    ubah dulu

--- covid.js
+++ covid.js
@@ -46,6 +46,18 @@ $(document).ready(function(){
                                     'Sembuh : '+sembuh+' Orang'
                                 )
                             }
+                            
+                            if(namaNegara === 'Indonesia'){
+                                var positif = dataNegara.cases;
+                                var mati = dataNegara.deaths;
+                                var sembuh = dataNegara.recovered;
+                                $('#indo').html(
+                                    $('#positif_indo').html(positif)+
+                                    $('#mati_indo').html(mati)+
+                                    $('#sembuh_indo').html(sembuh)
+                                )
+                            }
+
                             if(namaNegara === 'Malaysia'){
                                 var positif = dataNegara.cases;
                                 var mati = dataNegara.deaths;
@@ -54,6 +66,17 @@ $(document).ready(function(){
                                     'Positif : '+positif+' Orang <br>'+
                                     'Meninggal : '+mati+' Orang <br>'+
                                     'Sembuh : '+sembuh+' Orang'
+                                )
+                            }
+
+                            if(namaNegara === 'Malaysia'){
+                                var positif = dataNegara.cases;
+                                var mati = dataNegara.deaths;
+                                var sembuh = dataNegara.recovered;
+                                $('#malay').html(
+                                    $('#positif_malay').html(positif)+
+                                    $('#mati_malay').html(mati)+
+                                    $('#sembuh_malay').html(sembuh)
                                 )
                             }
 
@@ -67,24 +90,59 @@ $(document).ready(function(){
                                     'Sembuh : '+sembuh+' Orang'
                                 )
                             }
-                            if(namaNegara === 'Timor-Leste'){
+
+                            if(namaNegara === 'Singapore'){
                                 var positif = dataNegara.cases;
                                 var mati = dataNegara.deaths;
                                 var sembuh = dataNegara.recovered;
-                                $('#timor_leste').html(
+                                $('#singa').html(
+                                    $('#positif_singa').html(positif)+
+                                    $('#mati_singa').html(mati)+
+                                    $('#sembuh_singa').html(sembuh)
+                                )
+                            }
+
+                            if(namaNegara === 'Vietnam'){
+                                var positif = dataNegara.cases;
+                                var mati = dataNegara.deaths;
+                                var sembuh = dataNegara.recovered;
+                                $('#viet').html(
                                     'Positif : '+positif+' Orang <br>'+
                                     'Meninggal : '+mati+' Orang <br>'+
                                     'Sembuh : '+sembuh+' Orang'
                                 )
                             }
-                            if(namaNegara === 'Brunei'){
+
+                            if(namaNegara === 'Vietnam'){
                                 var positif = dataNegara.cases;
                                 var mati = dataNegara.deaths;
                                 var sembuh = dataNegara.recovered;
-                                $('#brunei').html(
+                                $('#vietnam').html(
+                                    $('#positif_vie').html(positif)+
+                                    $('#mati_vie').html(mati)+
+                                    $('#sembuh_vie').html(sembuh)
+                                )
+                            }
+
+                            if(namaNegara === 'Thailand'){
+                                var positif = dataNegara.cases;
+                                var mati = dataNegara.deaths;
+                                var sembuh = dataNegara.recovered;
+                                $('#thailand').html(
                                     'Positif : '+positif+' Orang <br>'+
                                     'Meninggal : '+mati+' Orang <br>'+
                                     'Sembuh : '+sembuh+' Orang'
+                                )
+                            }
+
+                            if(namaNegara === 'Thailand'){
+                                var positif = dataNegara.cases;
+                                var mati = dataNegara.deaths;
+                                var sembuh = dataNegara.recovered;
+                                $('#thail').html(
+                                    $('#positif_thai').html(positif)+
+                                    $('#mati_thai').html(mati)+
+                                    $('#sembuh_thai').html(sembuh)
                                 )
                             }
                         }
